a4c87de19c48c7fc8bca75a0096cc1f1f9d72f7d
satoshi.marumoto <satoshi.marumoto@st-ventures.jp>
2020-03-31 13:24:21 +0900

    UILongPressGestureSample

--- UILongPressGestureSample/ViewController.swift
+++ UILongPressGestureSample/ViewController.swift
@@ -9,12 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var label:UILabel!
+    
+    var count = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let tapGesture:UITapGestureRecognizer = UITapGestureRecognizer(
+            target: self,
+            action: #selector(ViewController.tapped(_:)))
+        tapGesture.delegate = self
+        view.addGestureRecognizer(tapGesture)
+        
+        // ロングプレス
+        let longPressGesture =
+            UILongPressGestureRecognizer(target: self,
+                                         action: #selector(ViewController.longPress(_:)))
+        longPressGesture.delegate = self
+        // Viewに追加.
+        view.addGestureRecognizer(longPressGesture)
     }
-
+    
+    @objc func tapped(_ sender: UITapGestureRecognizer){
+        if sender.state == .ended {
+            label.text = "Tapped ! \(count)"
+            count += 1
+        } else if sender.state == .began {
+            print("Tap began")
+        }
+    }
+    
+    @objc func longPress(_ sender: UILongPressGestureRecognizer){
+           if sender.state == .began {
+               print("LongPress began")
+           }
+           else if sender.state == .ended {
+            label.text = "Long Pressed !"
+        }
+    }
 
 }
 
+extension ViewController: UIGestureRecognizerDelegate {
+    
+}
